9dfe75cc88f0fa15ba84a362b9c577b2636c5f79
peetonn <gpeetonn@gmail.com>
2014-12-17 12:26:02 -0800

    fixed consumer to display video in canvas

--- consumer/index.js
+++ consumer/index.js
@@ -143,3 +143,36 @@ document.onkeypress = function (event){
 		break;
 	}
 }
+
+// video-to-canvas magic...
+// see more on http://html5hub.com/using-the-getusermedia-api-with-the-html5-video-and-canvas-elements/
+function setupVideoToCanvasPorcessing(){
+	var isStreaming = false,
+	v = document.getElementById('remote-video'),
+	c = document.getElementById('video-canvas'),
+	w = 600, 
+    h = 420,
+	con = c.getContext('2d');
+
+	v.addEventListener('canplay', function(e) {
+		if (!isStreaming) {
+      		// videoWidth isn't always set correctly in all browsers
+      		if (v.videoWidth > 0) h = v.videoHeight / (v.videoWidth / w);
+      		c.setAttribute('width', w);
+      		c.setAttribute('height', h);
+      		// Reverse the canvas image
+      		con.translate(w, 0);
+      		con.scale(-1, 1);
+      		isStreaming = true;
+  		}
+	}, false);
+	
+	v.addEventListener('play', function() {
+   		// Every 33 milliseconds copy the video image to the canvas
+   		setInterval(function() {
+   			if (v.paused || v.ended) return;
+   			con.fillRect(0, 0, w, h);
+   			con.drawImage(v, 0, 0, w, h);
+   		}, 33);
+	}, false);
+} // end of magic =(
