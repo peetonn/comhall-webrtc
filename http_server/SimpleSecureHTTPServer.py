d50519fa14bc9761721da0fbcbadf30a945fe68e
peetonn <gpeetonn@gmail.com>
2015-01-22 17:48:58 -0800

    fixed wrong variable name

--- http_server/SimpleSecureHTTPServer.py
+++ http_server/SimpleSecureHTTPServer.py
@@ -6,8 +6,9 @@ address='localhost'
 port=8000
 
 if len(sys.argv) == 3:
-	ip = sys.argv[1]
+	address = sys.argv[1]
 	port = int(sys.argv[2])
+	print 'starting on '+address+':'+str(port)
 
 httpd = BaseHTTPServer.HTTPServer((address, port), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='http_server/server.pem', server_side=True)
