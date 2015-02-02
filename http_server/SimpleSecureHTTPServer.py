65d7ea6a5e9a5abf693c3dedce1dc2d66a157867
peetonn <gpeetonn@gmail.com>
2015-02-02 10:35:02 -0800

    Revert "fixed wrong variable name"
    This reverts commit d50519fa14bc9761721da0fbcbadf30a945fe68e.

--- http_server/SimpleSecureHTTPServer.py
+++ http_server/SimpleSecureHTTPServer.py
@@ -6,9 +6,8 @@ address='localhost'
 port=8000
 
 if len(sys.argv) == 3:
-	address = sys.argv[1]
+	ip = sys.argv[1]
 	port = int(sys.argv[2])
-	print 'starting on '+address+':'+str(port)
 
 httpd = BaseHTTPServer.HTTPServer((address, port), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='http_server/server.pem', server_side=True)
